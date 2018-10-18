64666e6f7e26dc9aafd5c80f11a04f505d5aab36
April Copes <antdevelopment1@gmail.com>
2018-10-17 20:49:13 -0400

    Attempts to use try and catch on asynchronous javascript

---
+++ scripts/quiz3.js
@@ -0,0 +1,30 @@
+// Question 3
+// Instead of passing it up the stack throw it instead and try to catch it later on.
+
+// const fs = require("fs");
+
+// function readFileThenDo(next) {
+//   fs.readFile("./blah.nofile", (err, data) => {
+//     if (err) throw err;
+//     next(data);
+//   });
+// }
+// // Hint use try..catch
+// readFileThenDo(data => {
+//   console.log(data);
+// });
+
+// Question #3 Soultion
+// We can't use try catch with asynchronous javascript code. Trick question.
+const fs = require("fs");
+
+function readFileThenDo(next) {
+  fs.readFile("./blah.nofile", (err, data) => {
+    if (err) throw err;
+    next(data);
+  });
+}
+// Hint use try..catch
+readFileThenDo((err, data) => {
+  console.log(err, data);
+});
