b984f812472fb141a875a4d5480c1977be35055d
April Copes <antdevelopment1@gmail.com>
2018-10-21 15:40:10 -0400

    Solves quiz 1 promises

--- promises/index.js
+++ promises/index.js
@@ -1,35 +1,48 @@
-// We pass a an annonymous function with two parameters resolve and reject
-function doAsyncTask() {
-  let error = true;
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Async work completed.');
-      if (error) {
-        reject("Rejected");
+// // We pass a an annonymous function with two parameters resolve and reject
+// function doAsyncTask() {
+//   let error = true;
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('Async work completed.');
+//       if (error) {
+//         reject("Rejected");
+//       } else {
+//         resolve("Resolved");
+//       }
+//     }, 1000);
+//   }); 
+//   return promise;
+// }
+// doAsyncTask().then(
+//   (val) => console.log(val),
+//   (err) => console.log(err)
+// );
+
+
+// Create a promise version of the async readFile function
+
+const fs = require("fs");
+
+function readFile(filename, encoding) {
+  const promise = new Promise((sucess, failure) => {
+    fs.readFile(filename, encoding, (err, data) => {
+      if (err) {
+        failure(err);
       } else {
-        resolve("Resolved");
+        sucess(data);
       }
-    }, 1000);
-  }); 
+    });
+  });
   return promise;
 }
-doAsyncTask().then(
-  (val) => console.log(val),
-  (err) => console.log(err)
+
+readFile("./index.js", "utf-8").then(
+  (sucess) => console.log("File Read", sucess),
+  (failure) => console.log("Failed to Read", failure)
 );
 
-
-// // Create a promise version of the async readFile function
-
-// const fs = require("fs");
-
-// function readFile(filename, encoding) {
-//   fs.readFile(filename, encoding, (err, data) => {
-//     //TODO
-//   });
-// }
 // readFile("./files/demofile.txt", "utf-8", () => {
     
 // })
-// //     .then(...)
-// // });
+//     .then(...)
+// });
